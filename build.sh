7c85d29dc1bd635a9dec43a54202db8cc17128e3
Cuauh <72818099+CuauhComputer@users.noreply.github.com>
2022-10-13 08:49:52 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+# http(s) link to package home page.
+TERMUX_PKG_HOMEPAGE=https://www.cuauh.fr
+
+# One-line, short package description.
+TERMUX_PKG_DESCRIPTION="Convert .txt to .urt files"
+
+# License.
+TERMUX_PKG_LICENSE="GPL-2.0"
+
+# Who cares about package.
+# Specify yourself (Github nick, or name + email) if you wish to maintain the
+# package, fix its bugs, etc. Otherwise specify "@termux".
+# Please note that unofficial repositories are not allowed to reference @termux
+# as their maintainer.
+TERMUX_PKG_MAINTAINER="cuauh@cuauh.fr"
+
+# Version.
+TERMUX_PKG_VERSION=0.1
+
+# URL to archive with source code.
+TERMUX_PKG_SRCURL=https://www.cuauh.fr
+
+# SHA-256 checksum of the source code archive.
+TERMUX_PKG_SHA256=ad4489c0ad7a108c514262da28e6c2a426946fb408a3977ef1ed34308bdfd174
